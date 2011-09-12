fcd29934fd281f410823c97197e839e9148dcf18
Baishampayan Ghose <b.ghose@gmail.com>
2011-09-12 23:34:03 +0530

    New keybindings.

--- config/bindings.el
+++ config/bindings.el
@@ -3,7 +3,12 @@
 (global-set-key "\C-y" 'clipboard-yank)
 
 (global-set-key "\C-c\M-t" 'multi-term)
+(global-set-key (kbd "C-c t") 'multi-term-next)
+(global-set-key (kbd "C-c T") 'multi-term) ;; create a new one
 
 (global-set-key (kbd "<f5>") 'revert-all-buffers)
 
 (global-set-key "\C-xk" 'kill-current-buffer)
+
+(eval-after-load "magit"
+  (progn '(global-set-key (kbd "C-x g") 'magit-status)))
