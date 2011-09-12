40ee8a9467bf292ea0279e8a8f6ee258a2d7b1fa
Baishampayan Ghose <b.ghose@gmail.com>
2011-09-12 23:35:23 +0530

    Put the slime stuff inside an eval-after-load.

--- config/slime.el
+++ config/slime.el
@@ -1,4 +1,7 @@
 (add-lib-path "slime")
 (require 'slime)
-(slime-setup '(slime-repl slime-scratch slime-editing-commands))
-(setq slime-protocol-version 'ignore)
+
+(eval-after-load "slime"
+  '(progn
+     (slime-setup '(slime-repl slime-scratch slime-editing-commands))
+     (setq slime-protocol-version 'ignore)))
