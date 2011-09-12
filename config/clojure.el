cbb0c37b477a9f6779806ffcc3429381741c212a
Baishampayan Ghose <b.ghose@gmail.com>
2011-09-12 23:34:34 +0530

    Changed the way Lambdas are pretty-printed in Clojure.

--- config/clojure.el
+++ config/clojure.el
@@ -9,14 +9,14 @@
   '(font-lock-add-keywords
     'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
                      (0 (progn (compose-region (match-beginning 1)
-                                               (match-end 1) "λ")
+                                               (match-end 1) "ƒ")
                                nil))))))
 
 (eval-after-load 'clojure-mode
   '(font-lock-add-keywords
     'clojure-mode `(("\\(#\\)("
                      (0 (progn (compose-region (match-beginning 1)
-                                               (match-end 1) "ƒ")
+                                               (match-end 1) "λ")
                                nil))))))
 
 (eval-after-load 'clojure-mode
