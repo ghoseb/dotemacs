73320f4476a7cdf157eb2676e1936a28e3d0c333
Baishampayan Ghose <b.ghose@infinitelybeta.com>
2011-09-23 12:36:21 +0530

    Changed the default encoding system of SLIME to UTF-8

--- config/slime.el
+++ config/slime.el
@@ -4,4 +4,5 @@
 (eval-after-load "slime"
   '(progn
      (slime-setup '(slime-repl slime-scratch slime-editing-commands))
-     (setq slime-protocol-version 'ignore)))
+     (setq slime-protocol-version 'ignore)
+     (setq slime-net-coding-system 'utf-8-unix)))
