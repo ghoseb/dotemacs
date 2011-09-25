3b77fe93b59cbc9734d7c84f68e34ff3a9982f4b
Baishampayan Ghose <b.ghose@gmail.com>
2011-09-25 22:00:14 +0530

    Add new util-fn - sudo-edit.

--- config/utils.el
+++ config/utils.el
@@ -72,6 +72,12 @@
   (shell-command "wmctrl -r :ACTIVE: -btoggle,fullscreen"))
 
 
+(defun sudo-edit (&optional arg)
+  (interactive "p")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 ;;; ----------------------
 ;;; Bondage and discipline
 ;;; ----------------------
