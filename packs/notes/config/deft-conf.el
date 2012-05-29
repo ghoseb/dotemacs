9b52a5c5e83565245ee66cf08083930670797aa7
Baishampayan Ghose <b.ghose@gmail.com>
2012-05-29 13:13:46 +0530

    Move notes config to its proper place.

---
+++ packs/notes/config/deft-conf.el
@@ -0,0 +1,7 @@
+(require 'deft)
+
+(setq deft-extension "md")
+(setq deft-directory "~/Dropbox/Notes")
+(setq deft-text-mode 'markdown-mode)
+(global-set-key [f8] 'deft)
+(setq deft-auto-save-interval 10.0)
--- packs/notes/init.el
+++ packs/notes/init.el
@@ -1,6 +1,1 @@
-(require 'deft)
-
-(setq deft-extension "md")
-(setq deft-directory "~/Dropbox/Notes")
-(setq deft-text-mode 'markdown-mode)
-(global-set-key [f8] 'deft)
+(live-load-config-file "deft-conf.el")
