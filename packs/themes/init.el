b98ba8be9842b7d89794ef602f1a5429f8a686ce
Baishampayan Ghose <b.ghose@gmail.com>
2012-06-14 10:53:36 +0530

    Add comment for Zenburn users.
    Thanks to Johan Widén for the suggestion.

--- packs/themes/init.el
+++ packs/themes/init.el
@@ -1,3 +1,21 @@
 (setq custom-theme-directory (live-pack-lib-dir))
 
 (load-theme 'tomorrow-night t)
+
+;;; uncomment the following if you want zenburn
+;; (load-theme 'zenburn t)
+;; (set-cursor-color "firebrick")
+;; (setq hcz-set-cursor-color-color "")
+;; (setq hcz-set-cursor-color-buffer "")
+
+;; (defun my-set-cursor-color ()
+;;   "Change cursor color according to themes/init.el"
+;;   ;; set-cursor-color is somewhat costly, so we only call it when needed:
+;;   (let ((color "firebrick"))
+;;     (unless (and
+;;              (string= color hcz-set-cursor-color-color)
+;;              (string= (buffer-name) hcz-set-cursor-color-buffer))
+;;       (set-cursor-color (setq hcz-set-cursor-color-color color))
+;;       (setq hcz-set-cursor-color-buffer (buffer-name)))))
+
+;; (add-hook 'post-command-hook 'my-set-cursor-color)
