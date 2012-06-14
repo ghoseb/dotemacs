224707e7f4d1e2d060e21e81f2f45259d0dc9d36
Baishampayan Ghose <b.ghose@gmail.com>
2012-06-14 12:30:00 +0530

    Add more paredit key-bindings.

--- packs/clojure/config/paredit-conf.el
+++ packs/clojure/config/paredit-conf.el
@@ -1,5 +1,8 @@
-(eval-after-load 'paredit
-  ;; need a binding that works in the terminal
-  '(define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp))
-
 (require 'paredit)
+
+(eval-after-load 'paredit
+  '(progn
+     (define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp)
+     (define-key paredit-mode-map (kbd "M-(") 'paredit-wrap-round)
+     (define-key paredit-mode-map (kbd "M-[") 'paredit-wrap-square)
+     (define-key paredit-mode-map (kbd "M-{") 'paredit-wrap-curly)))
