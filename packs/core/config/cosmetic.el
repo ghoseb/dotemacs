a442315e14a1157d48b3909cd5fa30d1dea24711
Baishampayan Ghose <b.ghose@gmail.com>
2012-06-14 10:53:20 +0530

    Change the cursor shape depending on whether a mark is active.
    Thanks to Johan Widén for the suggestion.

--- packs/core/config/cosmetic.el
+++ packs/core/config/cosmetic.el
@@ -21,3 +21,16 @@
 (custom-set-variables
  '(term-default-bg-color "#000000")  ;; background color (black)
  '(term-default-fg-color "#dddd00")) ;; foreground color (yellow)
+
+
+;; Cursor-type
+;; Use a bar cursor when mark is active and a region exists.
+(defun th-activate-mark-init ()
+  (setq cursor-type 'bar))
+
+(add-hook 'activate-mark-hook 'th-activate-mark-init)
+
+(defun th-deactivate-mark-init ()
+  (setq cursor-type 'box))
+
+(add-hook 'deactivate-mark-hook 'th-deactivate-mark-init)
