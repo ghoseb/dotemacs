695dba50c90a5ef065ea3c7e0dccbea13fcabbe7
Baishampayan Ghose <b.ghose@gmail.com>
2013-01-19 14:29:06 +0530

    Fix indentation in SML mode.

--- packs/programming/config/sml-conf.el
+++ packs/programming/config/sml-conf.el
@@ -2,3 +2,6 @@
 
 (setenv "PATH" (concat "/usr/local/Cellar/smlnj/110.75/libexec/bin:" (getenv "PATH")))
 (setq exec-path (cons "/usr/local/Cellar/smlnj/110.75/libexec/bin"  exec-path))
+
+(eval-after-load 'sml-mode
+  '(define-key sml-mode-map (kbd "C-j") 'reindent-then-newline-and-indent))
