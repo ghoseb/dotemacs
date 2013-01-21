44795d4941c8d79123f3fe013bc2b5d6b7e2c8cf
Baishampayan Ghose <b.ghose@gmail.com>
2013-01-21 15:55:36 +0530

    Add Marmalade repo just in case.

--- packs/user/init.el
+++ packs/user/init.el
@@ -1,3 +1,8 @@
 (setq user-full-name "Baishampayan Ghose")
 (setq user-mail-address "b.ghose@helpshift.com")
 (set-default-font "Anonymous Pro-14")
+
+(require 'package)
+(add-to-list 'package-archives
+  '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
