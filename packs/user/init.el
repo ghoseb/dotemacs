ea01e2df332520680cd88dd3bfdd3a01ef062967
Baishampayan Ghose <b.ghose@gmail.com>
2013-02-03 11:04:53 +0530

    Use set-frame-font instead of set-default-font.

--- packs/user/init.el
+++ packs/user/init.el
@@ -1,6 +1,10 @@
 (setq user-full-name "Baishampayan Ghose")
 (setq user-mail-address "b.ghose@helpshift.com")
-(set-default-font "Anonymous Pro-14")
+
+;;; change this according to your needs
+(defvar default-font "Anonymous Pro-14" "My default Emacs font.")
+
+(set-frame-font default-font nil t)
 
 (require 'package)
 (add-to-list 'package-archives
