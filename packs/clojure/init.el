01333fafb02d0fc1dd7ad9df0f5906ba091445d6
Baishampayan Ghose <b.ghose@gmail.com>
2013-06-27 10:45:55 +0530

    Use nrepl by default.

--- packs/clojure/init.el
+++ packs/clojure/init.el
@@ -6,6 +6,6 @@
 (live-load-config-file "lisps-conf.el")
 (live-load-config-file "highlight-flash-conf.el")
 (live-load-config-file "clojure-conf.el")
-(live-load-config-file "slime-conf.el")
+;; (live-load-config-file "slime-conf.el")
 (live-load-config-file "auto-complete-conf.el")
-;; (live-load-config-file "nrepl.el")
+(live-load-config-file "nrepl.el")
