0c8bb53b3e7cc2f40970d92fad95725dc6be8c95
Baishampayan Ghose <b.ghose@gmail.com>
2013-08-05 14:22:53 +0530

    Save projectile bookmarks in etc dir.

--- packs/programming/config/projectile-conf.el
+++ packs/programming/config/projectile-conf.el
@@ -6,5 +6,11 @@
 ;;; https://github.com/magnars/s.el
 ;;; https://github.com/magnars/dash.el
 (require 'projectile)
+
 ;;; https://github.com/bbatsov/projectile#basic-setup
 (projectile-global-mode)
+
+(eval-after-load 'projectile
+  '(progn
+    (custom-set-variables
+     '(projectile-known-projects-file (concat live-etc-dir "projectile-bookmarks.eld")))))
