8f8d2cff08c23428b9bdf9e339579d582d21dda0
Baishampayan Ghose <b.ghose@gmail.com>
2013-08-15 19:12:45 +0530

    Remove redundant code.

--- packs/clojure/config/clojure-conf.el
+++ packs/clojure/config/clojure-conf.el
@@ -5,21 +5,16 @@
     'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
                      (0 (progn (compose-region (match-beginning 1)
                                                (match-end 1) "λ")
-                               nil))))))
-
-(eval-after-load 'clojure-mode
-  '(font-lock-add-keywords
-    'clojure-mode `(("\\(#\\)("
+                               nil)))
+                    ("\\(#\\)("
                      (0 (progn (compose-region (match-beginning 1)
                                                (match-end 1) "ƒ")
-                               nil))))))
-
-(eval-after-load 'clojure-mode
-  '(font-lock-add-keywords
-    'clojure-mode `(("\\(#\\){"
+                               nil)))
+                    ("\\(#\\){"
                      (0 (progn (compose-region (match-beginning 1)
                                                (match-end 1) "∈")
                                nil))))))
+
 
 (eval-after-load 'find-file-in-project
   '(add-to-list 'ffip-patterns "*.clj"))
