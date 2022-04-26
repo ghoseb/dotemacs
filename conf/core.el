896151a9e6a21956de71b7768e3b0669a86b4a40
Baishampayan Ghose <bg@teamohana.com>
2022-04-26 21:10:12 +0530

    fix: Unbreak mangled file

--- conf/core.el
+++ conf/core.el
@@ -6,12 +6,15 @@
 
 (use-package prescient
   :straight t
-  :config (prescient-persist-modcient
-  :after selectrum
+  :config (prescient-persist-mode +1))
+
+(use-package selectrum-prescient
   :straight t
+  :after selectrum
   :init (selectrum-prescient-mode +1))
 
 (use-package ctrlf
   :straight t
-  :init (ctrlf-mode +1))
+  :config (ctrlf-mode +1))
+
 
