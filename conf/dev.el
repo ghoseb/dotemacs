a9ff25002c93bf41193d552801c2c049cd129aa9
Baishampayan Ghose <bg@teamohana.com>
2022-04-27 11:49:53 +0530

    feat: Add lsp, but keep it disabled by default

--- conf/dev.el
+++ conf/dev.el
@@ -19,3 +19,27 @@
   (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode))
 
 
+(use-package lsp-mode
+  :straight t
+  :defer t
+  :ensure-system-package (clojure-lsp . "brew install clojure-lsp/brew/clojure-lsp-native")
+  :init (setq lsp-keymap-prefix "C-c l")
+  ;; :hook ((clojurescript-mode clojurec-mode clojure-mode) . lsp-deferred)
+  :custom
+  (lsp-lens-enable t)
+  (lsp-signature-auto-activate nil)
+  (lsp-eldoc-enable-hover nil)
+  (lsp-enable-indentation nil)
+  (lsp-enable-completion-at-point nil)
+  :commands (lsp lsp-deferred))
+
+
+(use-package lsp-ui
+  :straight t
+  :defer t
+  :commands lsp-ui-mode
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+  (setq lsp-ui-doc-position 'bottom)
+  :custom
+  (lsp-ui-sideline-enable nil))
