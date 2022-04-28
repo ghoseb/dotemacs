920541b16b8b8e5a9a04ebdfd5bda59db0768ca8
Baishampayan Ghose <bg@teamohana.com>
2022-04-28 21:53:56 +0530

    feat: Improve org mode and add Olivetti mode

--- conf/utils.el
+++ conf/utils.el
@@ -2,7 +2,13 @@
 
 (use-package org
   :straight t
-  :defer t)
+  :defer t
+  :config
+  (setq org-startup-indented t
+        org-pretty-entities t
+        org-hide-emphasis-markers t
+        org-startup-with-inline-images t
+        org-image-actual-width '(300)))
 
 (use-package org-superstar
   :straight t
@@ -12,9 +18,35 @@
   (setq inhibit-compacting-font-caches t) ; to prevent possible slowdown
   (setq org-superstar-special-todo-items t))
 
+(use-package org-appear
+  :after org
+  :straight t
+  :hook (org-mode . org-appear-mode))
+
 (use-package mixed-pitch
   :straight t
-  :hook
-  (org-mode . mixed-pitch-mode))
+  :hook (org-mode . mixed-pitch-mode))
+
+(use-package olivetti
+  :straight t
+  :blackout "OL"
+  :init
+  (setq olivetti-body-width .55)
+  :config
+  (defun distraction-free ()
+    "Distraction-free writing environment"
+    (interactive)
+    (if (equal olivetti-mode nil)
+        (progn
+          (window-configuration-to-register 1)
+          (delete-other-windows)
+          (text-scale-increase 2)
+          (olivetti-mode t))
+      (progn
+        (jump-to-register 1)
+        (olivetti-mode 0)
+        (text-scale-decrease 2))))
+  :bind
+  (("C-s-o" . distraction-free)))
 
 ;;; utils.el ends here
