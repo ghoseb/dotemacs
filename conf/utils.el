19029a7093d5ae1bbcd725e77aaffa06dc2deac2
Baishampayan Ghose <bg@teamohana.com>
2022-04-30 15:01:19 +0530

    feat: Add centaur-tabs-mode (disabled by default)

--- conf/utils.el
+++ conf/utils.el
@@ -99,4 +99,91 @@
   :straight t
   :defer t)
 
+
+(use-package centaur-tabs
+  :straight t
+  :commands (centaur-tabs-mode)
+  :hook
+  (dired-mode . centaur-tabs-local-mode)
+  (cider-repl-mode . centaur-tabs-local-mode)
+  :config
+  (setq centaur-tabs-style "bar")
+  (setq centaur-tabs-set-bar 'left)
+  (setq centaur-tabs-close-button "×")
+  (setq centaur-tabs-height 32)
+  (setq centaur-tabs-set-icons t)
+  (setq centaur-tabs-gray-out-icons 'buffer)
+  (setq centaur-tabs-cycle-scope 'tabs)
+  (setq centaur-tabs-enable-ido-completion nil)
+  (setq centaur-tabs-show-new-tab-button nil)
+  (centaur-tabs-group-by-projectile-project)
+  (centaur-tabs-change-fonts bg/variable-pitch-font bg/default-font-size)
+  (defun centaur-tabs-buffer-groups ()
+     (list
+      (cond
+       ((not (eq (file-remote-p (buffer-file-name)) nil))
+        "Remote")
+       ((or (string-equal "*" (substring (buffer-name) 0 1))
+            (memq major-mode '(magit-process-mode
+                               magit-status-mode
+                               magit-diff-mode
+                               magit-log-mode
+                               magit-file-mode
+                               magit-blob-mode
+                               magit-blame-mode)))
+        "Emacs")
+       ((derived-mode-p 'prog-mode)
+        "Editing")
+       ((derived-mode-p 'dired-mode)
+        "Dired")
+       ((memq major-mode '(helpful-mode
+                           help-mode))
+        "Help")
+       ((memq major-mode '(org-mode
+                           org-agenda-clockreport-mode
+                           org-src-mode
+                           org-agenda-mode
+                           org-beamer-mode
+                           org-indent-mode
+                           org-bullets-mode
+                           org-cdlatex-mode
+                           org-agenda-log-mode
+                           diary-mode))
+        "OrgMode")
+       (t
+        (centaur-tabs-get-group-name (current-buffer))))))
+
+  (defun centaur-tabs-hide-tab (x)
+    "Do no to show buffer X in tabs."
+    (let ((name (format "%s" x)))
+      (or
+       ;; Current window is not dedicated window.
+       (window-dedicated-p (selected-window))
+
+       ;; Buffer name not match below blacklist.
+       (string-prefix-p "*epc" name)
+       (string-prefix-p "*helm" name)
+       (string-prefix-p "*Helm" name)
+       (string-prefix-p "*Compile-Log*" name)
+       (string-prefix-p "*Async-native*" name)
+       (string-prefix-p "*lsp" name)
+       (string-prefix-p "*company" name)
+       (string-prefix-p "*Flycheck" name)
+       (string-prefix-p "*tramp" name)
+       (string-prefix-p " *Mini" name)
+       (string-prefix-p "*help" name)
+       (string-prefix-p "*straight" name)
+       (string-prefix-p " *temp" name)
+       (string-prefix-p "*Help" name)
+       (string-prefix-p "*mybuf" name)
+
+       ;; Is not magit buffer.
+       (and (string-prefix-p "magit" name)
+            (not (file-name-extension name))))))
+  :bind
+  ("C-<prior>" . centaur-tabs-backward)
+  ("C-<next>" . centaur-tabs-forward)
+  ("C-M-<prior>" . centaur-tabs-backward-group)
+  ("C-M-<next>" . centaur-tabs-forward-group))
+
 ;;; utils.el ends here
