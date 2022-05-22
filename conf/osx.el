75025397fe91ad9a58573c0902e910a4d3d2eb3e
Baishampayan Ghose <bg@teamohana.com>
2022-05-22 08:30:14 +0530

    build(osx): Use GNU `ls` for `dired` on MacOS if available

--- conf/osx.el
+++ conf/osx.el
@@ -3,3 +3,10 @@
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
 
+;; if `gnu ls' is in path, use that for dired
+(let ((bg--gnuls (executable-find "gls")))
+  (if bg--gnuls
+   (setq dired-use-ls-dired t
+         insert-directory-program bg--gnuls
+         dired-listing-switches "-aBhl --group-directories-first")
+   (setq dired-use-ls-dired nil)))
