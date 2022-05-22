a80cf6a75bfce6192fbb3d8d05a83efc4356972d
Baishampayan Ghose <bg@teamohana.com>
2022-05-22 20:24:12 +0530

    refactor: Cleanup some packages that were not being used

--- conf/utils.el
+++ conf/utils.el
@@ -40,60 +40,6 @@
   :straight t
   :hook (org-mode . org-appear-mode))
 
-
-(use-package mixed-pitch
-  :straight t
-  :after org
-  :hook (org-mode . mixed-pitch-mode))
-
-
-(use-package olivetti
-  :straight t
-  :after org
-  :blackout "OL"
-  :init
-  (setq olivetti-body-width .55)
-  :config
-  (defun distraction-free ()
-    "Distraction-free writing environment"
-    (interactive)
-    (if (equal olivetti-mode nil)
-        (progn
-          (window-configuration-to-register 1)
-          (delete-other-windows)
-          (text-scale-increase 2)
-          (olivetti-mode t))
-      (progn
-        (jump-to-register 1)
-        (olivetti-mode 0)
-        (text-scale-decrease 2))))
-  :bind
-  (("C-s-o" . distraction-free)))
-
-
-(use-package shackle
-  :straight t
-  :blackout
-  :init
-  (shackle-mode)
-  :custom
-  (shackle-rules '(("\\*Apropos\\|Help\\|Occur\\|tide-references\\*"
-                    :regexp t
-                    :same t
-                    :select t
-                    :inhibit-window-quit t)
-                   ("\\*magit" :regexp t :same t :select t :size 0.4)
-                   ("\\*shell.*" :regexp t :same t :select t)
-                   ("*Messages*" :select nil :other t)
-                   ("*Proced*" :select t :same t)
-                   ("*Buffer List*" :select t :same t)
-                   ("*Messages*" :same nil :other t :select t :inhibit-window-quit t)
-                   ;; clojure
-                   ("*sesman CIDER browser*" :inhibit-window-quit t :select t :same t)
-                   ("\\*cider-repl" :regexp t :same nil :other t)))
-  (shackle-default-rule nil))
-
-
 ;; `brew install libtool cmake' beforehand
 (use-package vterm
   :straight t
@@ -221,7 +167,7 @@
 
 (use-package keycast
   :straight t
-  :demand t
+  :commands bg/toggle-keycast
   :config
   (defun bg/toggle-keycast()
     (interactive)
