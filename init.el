2151de5a746d64420ad8bee6e00cd87b7e027607
Baishampayan Ghose <bg@teamohana.com>
2022-10-29 11:22:10 +0530

    feat!: Upgrade `straight.el`.

--- init.el
+++ init.el
@@ -7,15 +7,16 @@
 
 ;; initialise straight.el
 (setf straight-base-dir (expand-file-name "var/" bg/local-dir))
+(setf straight-repository-branch "develop")
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" straight-base-dir))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
