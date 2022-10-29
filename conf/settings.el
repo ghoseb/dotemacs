ff27df7e69b1942799377cfcdea59697a0c7154f
Baishampayan Ghose <bg@teamohana.com>
2022-10-29 09:26:55 +0530

    theme: Change default fonts to Cascadia & IBM Plex, theme to Kaolin

--- conf/settings.el
+++ conf/settings.el
@@ -3,15 +3,16 @@
 (setq user-full-name "Baishampayan Ghose"
       user-mail-address "b.ghose@gmail.com")
 
-(setq bg/default-font "Monaco")
-(setq bg/fixed-pitch-font "Monaco")
-(setq bg/variable-pitch-font "Gill Sans")
-(setq bg/default-font-size 140)
+(setq bg/default-font "Cascadia Code")
+(setq bg/variable-pitch-font "IBM Plex Sans")
+(setq bg/fixed-pitch-font bg/default-font)
+(setq bg/default-font-size 160)
 
 ;; Let us enable a nice font for Emojis
-(setq bg/emoji-font "Apple Color Emoji")
+(setq bg/emoji-font "Noto Emoji")
 
-(setq bg/save-dir (expand-file-name ".local/save" user-emacs-directory))
+(setq bg/dark-theme 'kaolin-dark)
+(setq bg/light-theme 'kaolin-light)
 
-(setq bg/default-theme 'doom-dracula)
-(setq bg/alternative-theme 'doom-nord-light)
+(setq bg/default-theme bg/dark-theme
+      bg/alternative-theme bg/light-theme)
--- conf/themes.el
+++ conf/themes.el
@@ -57,12 +57,63 @@
 
 (use-package doom-themes
   :straight t
+  :disabled t
   :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
   (setq doom-themes-treemacs-theme "doom-colors")
   (doom-themes-visual-bell-config)
   (doom-themes-treemacs-config)
-  (doom-themes-org-config)
-  :init
-  (load-theme bg/default-theme t))
+  (doom-themes-org-config))
+
+(use-package kaolin-themes
+  :straight t
+  :demand t
+  :custom
+  (kaolin-themes-bold t)
+  (kaolin-themes-italic t)
+  (kaolin-themes-underline nil)
+  (kaolin-themes-italic-comments t)
+  (kaolin-themes-distinct-fringe t)
+  (kaolin-themes-git-gutter-solid t)
+  (kaolin-themes-treemacs-hl-line t)
+  :config
+  (kaolin-treemacs-theme))
+
+
+(defun bg/apply-theme (appearance)
+  "Load theme, taking current system APPEARANCE into consideration."
+  (mapc #'disable-theme custom-enabled-themes)
+  (pcase appearance
+    ('light (load-theme bg/light-theme t))
+    ('dark (load-theme bg/dark-theme t))))
+
+;; (add-hook 'ns-system-appearance-change-functions #'bg/apply-theme)
+(load-theme bg/default-theme t)
+
+(use-package ligature
+  :straight (ligature :type git :host github :repo "mickeynp/ligature.el")
+  :config
+  ;; Enable the "www" ligature in every possible major mode
+  (ligature-set-ligatures 't '("www"))
+  ;; Enable traditional ligature support in eww-mode, if the
+  ;; `variable-pitch' face supports it
+  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+  ;; Enable all Cascadia Code ligatures in programming modes
+  (ligature-set-ligatures 'prog-mode
+                          '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+                            ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+                            "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+                            "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+                            "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+                            "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+                            "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                            "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+                            ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+                            "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+                            "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+                            "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+                            "\\\\" "://"))
+  ;; Enables ligature checks globally in all buffers. You can also do it
+  ;; per mode with `ligature-mode'.
+  (global-ligature-mode t))
