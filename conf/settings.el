b5d7ec16037c3f50c095db6738cbe3d31a899154
Baishampayan Ghose <bg@teamohana.com>
2023-08-06 10:04:06 +0530

    feat: Switch default typeface to Berkeley Mono

--- conf/settings.el
+++ conf/settings.el
@@ -3,10 +3,10 @@
 (setq user-full-name "Baishampayan Ghose"
       user-mail-address "b.ghose@gmail.com")
 
-(defvar bg--default-font "Cascadia Code" "The default font.")
+(defvar bg--default-font "Berkeley Mono" "The default font.")
 (defvar bg--variable-pitch-font "IBM Plex Sans" "The default variable pitch font.")
 (defvar bg--fixed-pitch-font bg--default-font "The default fixed pitch font.")
-(defvar bg--default-font-size 160 "The default font size.")
+(defvar bg--default-font-size 150 "The default font size.")
 
 ;; Let us enable a nice font for Emojis
 (defvar bg--emoji-font "Noto Emoji" "The font for emojis.")
