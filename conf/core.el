0704356a6433dd775885fdae51e5a3c832f53ffe
Baishampayan Ghose <bg@teamohana.com>
2024-12-15 12:47:02 +0530

    feat: Better `C-g` handling via `prot/keyboard-quit-dwim`

--- conf/core.el
+++ conf/core.el
@@ -60,7 +60,32 @@
   (setq minibuffer-prompt-properties
         '(read-only t cursor-intangible t face minibuffer-prompt))
   (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  (defun prot/keyboard-quit-dwim ()
+    "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'."
+    (interactive)
+    (cond
+     ((region-active-p)
+      (keyboard-quit))
+     ((derived-mode-p 'completion-list-mode)
+      (delete-completion-window))
+     ((> (minibuffer-depth) 0)
+      (abort-recursive-edit))
+     (t
+      (keyboard-quit))))
   :bind
+  ("C-g" . #'prot/keyboard-quit-dwim)
   ("C-c q" . #'bury-buffer)
   ("<escape>" . #'keyboard-escape-quit))
 
