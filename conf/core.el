06f2216c25ba3b9dd2e963a6d73e67bde83d04bd
Baishampayan Ghose <bg@teamohana.com>
2025-01-31 16:15:16 +0530

    chore: Reindent docstring inside function.

--- conf/core.el
+++ conf/core.el
@@ -64,16 +64,16 @@
   (defun prot/keyboard-quit-dwim ()
     "Do-What-I-Mean behaviour for a general `keyboard-quit'.
 
-The generic `keyboard-quit' does not do the expected thing when
-the minibuffer is open.  Whereas we want it to close the
-minibuffer, even without explicitly focusing it.
+    The generic `keyboard-quit' does not do the expected thing when
+    the minibuffer is open.  Whereas we want it to close the
+    minibuffer, even without explicitly focusing it.
 
-The DWIM behaviour of this command is as follows:
+    The DWIM behaviour of this command is as follows:
 
-- When the region is active, disable it.
-- When a minibuffer is open, but not focused, close the minibuffer.
-- When the Completions buffer is selected, close it.
-- In every other case use the regular `keyboard-quit'."
+      - When the region is active, disable it.
+      - When a minibuffer is open, but not focused, close the minibuffer.
+      - When the Completions buffer is selected, close it.
+      - In every other case use the regular `keyboard-quit'."
     (interactive)
     (cond
      ((region-active-p)
