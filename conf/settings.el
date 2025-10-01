fa90223ec9a44acaac734f38fe31aa1d462aafcf
Baishampayan Ghose <bg@teamohana.com>
2025-10-01 12:49:46 +0530

    fix: Change primary typeface name.

--- conf/settings.el
+++ conf/settings.el
@@ -3,7 +3,7 @@
 (setq user-full-name "Baishampayan Ghose"
       user-mail-address "b.ghose@gmail.com")
 
-(defvar bg--default-font "Berkeley Mono SemiCondensed" "The default font.")
+(defvar bg--default-font "Berkeley Mono" "The default font.")
 (defvar bg--variable-pitch-font "IBM Plex Sans" "The default variable pitch font.")
 (defvar bg--fixed-pitch-font bg--default-font "The default fixed pitch font.")
 (defvar bg--default-font-size 170 "The default font size.")
