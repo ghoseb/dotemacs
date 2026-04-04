126217afcf174cc4915c943e2a638beec1468fd6
Baishampayan Ghose <bg@teamohana.com>
2026-04-04 17:38:15 +0530

    style(magit): Refine font faces.

--- conf/dev.el
+++ conf/dev.el
@@ -42,12 +42,8 @@
   (magit-define-global-key-bindings nil)
   (magit-log-arguments '("--graph" "--decorate" "--color"))
   :config
-  (let ((sans-serif-family (face-attribute 'variable-pitch :family)))
-    (set-face-attribute 'magit-diff-file-heading nil :family sans-serif-family :weight 'normal :bold nil)
-    (set-face-attribute 'magit-diff-file-heading-highlight nil :family sans-serif-family :weight 'normal :bold nil)
-    (set-face-attribute 'magit-section-child-count nil :family sans-serif-family :weight 'normal :bold nil)
-    (set-face-attribute 'magit-section-heading nil :family sans-serif-family :bold t)
-    (set-face-attribute 'magit-section-highlight nil :family sans-serif-family :bold t))
+  (dolist (face '(magit-hash magit-blame-hash))
+    (set-face-attribute face nil :family bg--monaspace-keyword-font))
   (add-hook 'git-commit-setup-hook (lambda () (corfu-mode -1)))
   :bind
   ("C-x g" . magit-status))
